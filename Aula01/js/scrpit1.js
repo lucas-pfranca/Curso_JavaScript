9b2d96f8ecb71560edac1a98016469bb02deec47
LUCAS PENALVA DE FRANCA <lucas.pfranca@senacsp.edu.br>
2020-03-04 08:52:58 -0300

    Aula01

---
+++ Aula01/js/scrpit1.js
@@ -0,0 +1,6 @@
+// Exibir uma mensagem de boas vindas para o usuário
+alert("Olá, seja bem vindo!");
+
+var nome = prompt("Digite o seu nome");
+
+document.getElementById("título").innerHTML = nome;
