d02019cf39a47d5932211be27bd51b8ef328f76e
LUCAS PENALVA DE FRANCA <lucas.pfranca@senacsp.edu.br>
2020-03-13 10:05:58 -0300

    Ajustes nos script anonima e no index 02

--- Aula05/js/anonima01.js
+++ Aula05/js/anonima01.js
@@ -23,25 +23,57 @@ for (var p = 1; p <= 25; p++) {
     "<a href=# onclick='pg(" + p + ")'>" + p + "</a>";
 }
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
+function pg(n) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
 
-    var exibir = "<div>";
-    for (var i = 0; i <= 19; i++) {
-      exibir += "<img src=" + caminho + myObj.results[i].poster_path + ">";
+      var exibir = "";
+      for (var i = 0; i <= 19; i++) {
+        var barra = "<div>";
+
+        for (v = 1; v <= myObj.results[i].vote_average; v++) {
+          barra =
+            "<p style=background-color:red;color:white;width:" +
+            v * 50 +
+            "px>" +
+            myObj.results[i].vote_average +
+            "%</p>";
+        }
+
+        barra += "</div>";
+
+        exibir +=
+          "<div><img src=" +
+          caminho +
+          myObj.results[i].backdrop_path +
+          ">" +
+          "<h2>" +
+          myObj.results[i].title +
+          "</h2>" +
+          "<h3>Lançamento:" +
+          myObj.results[i].release_date +
+          "</h3>" +
+          "<p class=texto>" +
+          myObj.results[i].overview +
+          "</p>" +
+          "<p>" +
+          barra +
+          "</p></div>;";
+      }
+
+      document.getElementById("saida").innerHTML = exibir;
+      console.log(myObj.results[0].title);
+    } else {
+      console.log("Não foi possível obter os dados");
     }
-    exibir += "</div>;";
-    document.getElementById("saida").innerHTML = exibir;
-    console.log(myObj.results[0].title);
-  } else {
-    console.log("Não foi possível obter os dados");
-  }
-};
-xmlhttp.open(
-  "GET",
-  "https://api.themoviedb.org/3/movie/popular?api_key=7b5035a9cc58e07499509cdc63a19d38&language=pt-BR&page=3",
-  true
-);
-xmlhttp.send();
+  };
+  xmlhttp.open(
+    "GET",
+    "https://api.themoviedb.org/3/movie/popular?api_key=7b5035a9cc58e07499509cdc63a19d38&language=pt-BR&page=" +
+      n,
+    true
+  );
+  xmlhttp.send();
+}
